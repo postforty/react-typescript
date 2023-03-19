7e77f61852191199612494dfc8e2a54425a10416
postforty <75767073+postforty@users.noreply.github.com>
2023-03-19 21:07:57 +0900

    [ADD] 컴포넌트 추가

---
+++ ch04/ch04_1/src/pages/Clock.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react";
+import { Div, Title, Subtitle } from "../components";
+
+export type ClockProps = {
+  today: Date;
+};
+
+const Clock: FC<ClockProps> = ({ today }) => {
+  return (
+    <Div className="flex flex-col items-center justify-center h-screen bg-primary text-white"></Div>
+  );
+};
