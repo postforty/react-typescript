b09ebd64af7dbc22c1ece1790595d9cac5bdfe39
postforty <75767073+postforty@users.noreply.github.com>
2023-03-19 23:25:31 +0900

    [UPDATE] useState hook

--- ch04/ch04_1/src/App.tsx
+++ ch04/ch04_1/src/App.tsx
@@ -1,16 +1,18 @@
-import {useEffect, useRef} from 'react'
+import {useEffect, useState} from 'react'
 import Clock from './pages/Clock'
 
 export default function App() {
-  let today = useRef(new Date())
+  // let today = useRef(new Date())
+  const [today, setToday] = useState(new Date())
   useEffect(() => {
     console.log('useEffect called.')
     const duration = 1000
     const id = setInterval(() => {
-      today.current = new Date()
-      console.log('today', today.current.toLocaleTimeString())
+      setToday(new Date())
+      // today.current = new Date()
+      // console.log('today', today.current.toLocaleTimeString())
     }, duration)
     return () => clearInterval(id)
   }, [])
-  return <Clock today={today.current} />
+  return <Clock today={today} />
 }
