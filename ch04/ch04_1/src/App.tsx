f47d2b4cb8ec31f2fc43b10492b1eadeb91a4aa4
postforty <75767073+postforty@users.noreply.github.com>
2023-03-19 22:02:23 +0900

    [UPDATE] useRef

--- ch04/ch04_1/src/App.tsx
+++ ch04/ch04_1/src/App.tsx
@@ -1,6 +1,16 @@
+import {useEffect, useRef} from 'react'
 import Clock from './pages/Clock'
 
 export default function App() {
-  let today = new Date()
-  return <Clock today={today} />
+  let today = useRef(new Date())
+  useEffect(() => {
+    console.log('useEffect called.')
+    const duration = 1000
+    const id = setInterval(() => {
+      today.current = new Date()
+      console.log('today', today.current.toLocaleTimeString())
+    }, duration)
+    return () => clearInterval(id)
+  }, [])
+  return <Clock today={today.current} />
 }
