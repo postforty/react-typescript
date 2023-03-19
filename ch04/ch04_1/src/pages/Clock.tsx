15000f82d6f56b5ed98f7115e278303c996c75a3
postforty <75767073+postforty@users.noreply.github.com>
2023-03-19 21:22:27 +0900

    [ADD] 실습 파일 추가

--- ch04/ch04_1/src/pages/Clock.tsx
+++ ch04/ch04_1/src/pages/Clock.tsx
@@ -1,12 +1,14 @@
-import type { FC } from "react";
-import { Div, Title, Subtitle } from "../components";
+import type {FC} from 'react'
+import {Div, Title, Subtitle} from '../components'
 
 export type ClockProps = {
-  today: Date;
-};
+  today: Date
+}
 
-const Clock: FC<ClockProps> = ({ today }) => {
+const Clock: FC<ClockProps> = ({today}) => {
   return (
     <Div className="flex flex-col items-center justify-center h-screen bg-primary text-white"></Div>
-  );
-};
+  )
+}
+
+export default Clock
