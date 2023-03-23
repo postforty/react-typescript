e6ca91a91c3b0832aa1069ff8eaa5d7be3490599
postforty <75767073+postforty@users.noreply.github.com>
2023-03-23 22:54:12 +0900

    [ADD] useToggle 커스텀 훅

--- ch04/ch04_3/src/pages/ShowHideModal.tsx
+++ ch04/ch04_3/src/pages/ShowHideModal.tsx
@@ -1,8 +1,39 @@
-export default function CopyMe() {
+import {Subtitle, Title} from '../components'
+import {Button, Modal, ModalAction, ModalContent} from '../theme/daisyui'
+import * as D from '../data'
+import {useToggle} from '../hooks'
+import {useCallback} from 'react'
+
+export default function ShowHideModal() {
+  const [open, toggleOpen] = useToggle(false)
+  const onAccept = useCallback(() => {
+    toggleOpen()
+  }, [toggleOpen])
+
   return (
     <section className="mt-4">
-      <h2 className="font-bold text-5xl text-center">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>ShowHideModal</Title>
+      <div className="flex justify-center p-4">
+        <Button className="btn-primary" onClick={toggleOpen}>
+          open modal
+        </Button>
+      </div>
+      <Modal open={open}>
+        <ModalContent
+          closeIconClassName="btn-primary btn-outline"
+          onCloseIconClicked={toggleOpen}>
+          <Subtitle>Modal</Subtitle>
+          <p>{D.randomParagraphs()}</p>
+          <ModalAction>
+            <button className="btn btn-primary" onClick={onAccept}>
+              Accept
+            </button>
+            <button className="btn" onClick={toggleOpen}>
+              Close
+            </button>
+          </ModalAction>
+        </ModalContent>
+      </Modal>
     </section>
   )
 }
