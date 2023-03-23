b09c6c80f39dd58082116f3596aab4d0a740dbad
Dandy Code! <postforty@naver.com>
2023-03-23 09:43:05 +0900

    [UPDATE] tailwind 적용

--- ch04/ch04_3/src/pages/InputTest.tsx
+++ ch04/ch04_3/src/pages/InputTest.tsx
@@ -1,15 +1,15 @@
-import { ChangeEvent, useCallback, useState } from "react";
+import {ChangeEvent, useCallback, useState} from 'react'
 
 export default function InputTest() {
-  const [value, setValue] = useState<string>("");
-  const [checked, setChecked] = useState<boolean>(false);
+  const [value, setValue] = useState<string>('')
+  const [checked, setChecked] = useState<boolean>(false)
 
   const onChangeValue = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setValue((notUsed) => e.target.value);
-  }, []);
+    setValue(notUsed => e.target.value)
+  }, [])
   const onChangeChecked = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setChecked((notUsed) => e.target.checked);
-  }, []);
+    setChecked(notUsed => e.target.checked)
+  }, [])
   return (
     <section className="mt-4">
       <div className="flex items-center justify-center p-4 mt-4">
@@ -27,5 +27,5 @@ export default function InputTest() {
         />
       </div>
     </section>
-  );
+  )
 }
