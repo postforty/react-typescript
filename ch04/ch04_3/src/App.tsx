da5e5b8b7a604b878a911c371dfd89e6e714a30b
Dandy Code! <postforty@naver.com>
2023-03-23 09:27:05 +0900

    [ADD] useState

---
+++ ch04/ch04_3/src/App.tsx
@@ -0,0 +1,25 @@
+import ArrayState from "./pages/ArrayState";
+import ObjectState from "./pages/ObjectState";
+import BasicForm from "./pages/BasicForm";
+import HigherOrderRadioInputTest from "./pages/HigherOrderRadioInputTest";
+import RadioInputTest from "./pages/RadioInputTest";
+import ShowHideModal from "./pages/ShowHideModal";
+import InputTest from "./pages/InputTest";
+import NumberState from "./pages/NumberState";
+
+function App() {
+  return (
+    <main>
+      <ArrayState />
+      <ObjectState />
+      <BasicForm />
+      <HigherOrderRadioInputTest />
+      <RadioInputTest />
+      <ShowHideModal />
+      <InputTest />
+      <NumberState />
+    </main>
+  );
+}
+
+export default App;
