b3df4e5b4d87cb00f26ee6460b27861294fe2d68
postforty <75767073+postforty@users.noreply.github.com>
2023-03-28 22:56:54 +0900

    [ADD] 배열 타입 값일 때 useState 훅

--- ch04/ch04_3/src/pages/ObjectState.tsx
+++ ch04/ch04_3/src/pages/ObjectState.tsx
@@ -19,6 +19,11 @@ export default function ObjectState() {
     [form]
   )
 
+  // 대다수 프로그래밍 언어에서 어떤 변수에 담긴 값을 다른 변수에 복사할 때는 깊은 복사, 얕은 복사라는 2가지 방식 지원.
+  // 복사 방식은 값의 타입에 따라 각각 다르게 적용됨.
+  // 만일 number, boolean 등 값의 메모리 크기를 컴파일 타임 때 알 수 있는 타입은 항상 깊은 복사.
+  // 반면 객체, 배열 등 값의 메모리 크기를 런타임 때 알 수 있은 타입은 얕은 복사.
+  // string 타입 문자열은 항상 읽기 전용이므로 메모리 크기를 컴파일 타임 때 알 수 있음.
   const onChangeName = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setForm(state => ({...state, name: e.target.value}))
   }, [])
