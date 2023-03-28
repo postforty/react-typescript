c0bd0a7ba68837fbbcf5e73577296ecb21e20da2
Dandy Code! <postforty@naver.com>
2023-03-28 09:27:10 +0900

    FormData 클래스

--- ch04/ch04_3/src/pages/BasicForm.tsx
+++ ch04/ch04_3/src/pages/BasicForm.tsx
@@ -1,8 +1,71 @@
-export default function CopyMe() {
+import type {ChangeEvent, FormEvent} from 'react'
+import {useCallback, useState} from 'react'
+import {Title} from '../components'
+
+export default function BasicForm() {
+  const [name, setName] = useState<string>('')
+  const [email, setEmail] = useState<string>('')
+
+  const onSubmit = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault() // 웹페이지 리렌더링 방지
+
+      const formData = new FormData() // FormData는 자바스크립트 엔진이 기본으로 제공하는 클래스, 사용자가 입력한 데이터를 웹 서버에 전송할 목적
+      formData.append('name', name)
+      formData.append('email', email)
+      alert(JSON.stringify(Object.fromEntries(formData), null, 2))
+    },
+    [name, email]
+  )
+
+  const onChangeName = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setName(notUsed => e.target.value)
+  }, [])
+  const onChangeEmail = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(notUsed => e.target.value)
+  }, [])
+
   return (
     <section className="mt-4">
-      <h2 className="font-bold text-5xl text-center">CopyMe</h2>
-      <div className="mt-4"></div>
+      <Title>BasicForm</Title>
+      <div className="flex justify-center mt-4">
+        <form onSubmit={onSubmit}>
+          <div className="form-control">
+            <label className="label" htmlFor="name">
+              <span className="label-text">Username</span>
+            </label>
+            <input
+              value={name}
+              onChange={onChangeName}
+              id="name"
+              type="text"
+              placeholder="enter your name"
+              className="input input-primary"
+            />
+          </div>
+          <div className="form-control">
+            <label className="label" htmlFor="email">
+              <span className="label-text">email</span>
+            </label>
+            <input
+              value={email}
+              onChange={onChangeEmail}
+              id="email"
+              type="email"
+              placeholder="enter your email"
+              className="input input-primary"
+            />
+          </div>
+          <div className="flex justify-center mt-4">
+            <input
+              type="submit"
+              value="Submit"
+              className="w-1/2 btn btn-sm btn-primary"
+            />
+            <input defaultValue="Cancel" className="w-1/2 ml-4 btn btn-sm" />
+          </div>
+        </form>
+      </div>
     </section>
   )
 }
