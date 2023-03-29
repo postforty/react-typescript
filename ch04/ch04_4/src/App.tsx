ab95d81adc5aeecac0131e4d86d4e527bec3cb0b
postforty <75767073+postforty@users.noreply.github.com>
2023-03-30 00:12:34 +0900

    [INITIAL] useEffect, useLayoutEffect

---
+++ ch04/ch04_4/src/App.tsx
@@ -0,0 +1,15 @@
+import ClassLifecycle from './pages/ClassLifecycle'
+import FetchTest from './pages/FetchTest'
+import WindowResizeTest from './pages/WindowResizeTest'
+
+function App() {
+  return (
+    <main>
+      <FetchTest />
+      <WindowResizeTest />
+      <ClassLifecycle />
+    </main>
+  )
+}
+
+export default App
