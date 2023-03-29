6957ce5be3104dcdf9dca7c18091e89d047cadbb
postforty <75767073+postforty@users.noreply.github.com>
2023-03-30 00:19:07 +0900

    Update ClassLifecycle.tsx

--- ch04/ch04_4/src/pages/ClassLifecycle.tsx
+++ ch04/ch04_4/src/pages/ClassLifecycle.tsx
@@ -1,8 +1,20 @@
-export default function CopyMe() {
-  return (
-    <section className="mt-4">
-      <h2 className="font-bold text-5xl text-center">CopyMe</h2>
-      <div className="mt-4"></div>
-    </section>
-  )
+import {Component} from 'react'
+import {Title} from '../components'
+
+export default class ClassLifecycle extends Component {
+  state = {
+    today: new Date()
+  }
+  render() {
+    const {today} = this.state
+    return (
+      <section className="mt-4">
+        <Title>ClassLifecycle</Title>
+        <div className="mt-4 flex flex-col items-center">
+          <p className="font-mono text-3xl">{today.toLocaleDateString()}</p>
+          <p className="font-mono text-3xl">{today.toLocaleTimeString()}</p>
+        </div>
+      </section>
+    )
+  }
 }
