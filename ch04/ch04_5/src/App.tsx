d8ea9582215c717cd3a736f292402eb9f1b24e91
postforty <75767073+postforty@users.noreply.github.com>
2023-04-05 23:56:25 +0900

    [INITIAL] useRef, useImperativeHandle

---
+++ ch04/ch04_5/src/App.tsx
@@ -0,0 +1,20 @@
+import ValidatableInputTest from './pages/ValidatableInputTest'
+import ForwardRefTest from './pages/ForwardRefTest'
+import InputValueTest from './pages/InputValueTest'
+import InputFocusTest from './pages/InputFocusTest'
+import FileDrop from './pages/FileDrop'
+import ClickTest from './pages/ClickTest'
+function App() {
+  return (
+    <main>
+      <ValidatableInputTest />
+      <ForwardRefTest />
+      <InputValueTest />
+      <InputFocusTest />
+      <FileDrop />
+      <ClickTest />
+    </main>
+  )
+}
+
+export default App
