5bf38051832f22170eb2136a2befb1dd2c3fdb37
Dandy Code! <postforty@naver.com>
2023-04-06 09:05:55 +0900

    Update ClickTest.tsx

--- ch04/ch04_5/src/pages/ClickTest.tsx
+++ ch04/ch04_5/src/pages/ClickTest.tsx
@@ -11,6 +11,9 @@ export default function ClickTest() {
         <button className="mr-4 btn btn-primary" onClick={onClick}>
           Click Me
         </button>
+        {/* 모든 리액트 컴포넌트는 reference의 앞 3글자를 딴 ref 속성을 제공.
+        ref 속성은 초기에는 null이었다가 마운트되는 시점에서 물리 DOM 객체의 값이 됨.
+        즉, ref는 물리 DOM의 객체의 참조. */}
         <input ref={inputRef} className="hidden" type="file" accept="image/*" />
       </div>
     </section>
